a32e2f09fbafabe1c22e227b73b83b4b6ced8e3d
盧柏嘉 <lupochia@LuPaul.local>
2017-03-29 19:55:55 +0800

    dependent  topic has_many comment destroy

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_many :topic_categoryships
   has_many :categories, through: :topic_categoryships
