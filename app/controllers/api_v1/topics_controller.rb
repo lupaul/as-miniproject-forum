47d5dee724ebf8e2d1ec90f98a942d7551433aa2
盧柏嘉 <lupochia@LuPaul.local>
2017-04-15 11:54:38 +0800

    change api controller index

--- app/controllers/api_v1/topics_controller.rb
+++ app/controllers/api_v1/topics_controller.rb
@@ -3,10 +3,12 @@ class ApiV1::TopicsController <  ApiController
   # GET http://localhost:3000/api/v1/topics.json
   def index
     @topics = Topic.all
-    
+
     @header = request.headers.to_h["HTTP_TEST"]
     # render json: @topics.to_json
-    render json: {message: "#{@header}"}
+    
+    #測試header
+    # render json: {message: "#{@header}"}
   end
 
   #GET http://localhost:3000/api/v1/topics/21.json
